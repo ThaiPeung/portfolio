b18c580c05db7bf164bf5e21643f85d7e33f40da
OITPIO <miwot1234@gmail.com>
2025-03-12 11:19:17 +0700

    Add more dependencies

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <Canvas>
+            
+        </Canvas>
+      
+    </div>
+  )
+}
+
+export default Home
