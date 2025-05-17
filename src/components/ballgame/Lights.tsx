6d973275473ce945f2cc27148bd7aa3eb40c2785
ThaiPeung <miwot1234@gmail.com>
2025-05-17 17:36:10 +0700

    Update light to render shadow properly

--- src/components/ballgame/Lights.tsx
+++ src/components/ballgame/Lights.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 const Lights = () => {
+  const light = useRef<any>(null);
+
+  useFrame((state, delta) => {
+    if (light.current) {
+      // -| Update light position and target based on camera position and target to render shadow based on where player is, 
+      // -| if shadow is too far from light the shadow not rendered and if the light isn't move following camera, the shadow will be missing
+      // -| when move away from starting point.
+      // -| "- 4" to adjust light to move ahead of player current position because there is shadow rendered behide the camera and 
+      // -| it's a bit waste.
+      light.current.position.z = state.camera.position.z + 1 - 4;
+      light.current.target.position.z = state.camera.position.z - 4;
+      light.current.target.updateMatrixWorld();
+    }
+  });
+
   return (
     <>
       <directionalLight
+        ref={light}
         castShadow
         position={[4, 4, 1]}
         intensity={4.5}
